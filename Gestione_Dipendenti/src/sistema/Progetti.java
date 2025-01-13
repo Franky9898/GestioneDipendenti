684ab278aa38b939d5838a157fa0b860ffea431f
HazemRaa <haz3m95@gmail.com>
2025-01-13 16:41:06 +0100

    commit class project

--- Gestione_Dipendenti/src/sistema/Progetti.java
+++ Gestione_Dipendenti/src/sistema/Progetti.java
@@ -2,5 +2,9 @@ package sistema;
 
 public class Progetti
 {
-
+	public static void Progetto() {}
+	private int idProgetto;
+	private String nomeProgetto;
+	private String idprojectManager;
+	private String idteamAssegnati;
 }
