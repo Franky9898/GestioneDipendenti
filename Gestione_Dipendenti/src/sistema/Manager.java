ded8b4b4ee348d6532737dd21a0d0961a45f62b4
Giusy-Paccione <giusy_paccione@outlook.it>
2025-01-15 16:07:19 +0100

    manager primo commit

--- Gestione_Dipendenti/src/sistema/Manager.java
+++ Gestione_Dipendenti/src/sistema/Manager.java
@@ -1,6 +1,47 @@
 package sistema;
 
-public class Manager extends Dipendenti
-{
+import java.sql.Connection;
+import java.util.Scanner;
 
+public class Manager extends Dipendenti {
+	private int id;
+	private int idDipendente;
+	private double bonus;
+	private int idTeamGestito;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdDipendente() {
+		return idDipendente;
+	}
+	public void setIdDipendente(int idDipendente) {
+		this.idDipendente = idDipendente;
+	}
+	public double getBonus() {
+		return bonus;
+	}
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	public int getIdTeamGestito() {
+		return idTeamGestito;
+	}
+	public void setIdTeamGestito(int idTeamGestito) {
+		this.idTeamGestito = idTeamGestito;
+	}
+	
+	public static void visualizzaManager(Connection conn, Scanner scanner) {
+		
+		String query ="SELECT nome, cognome, manager.idTeamGestito AS team\n"
+				+ "FROM azienda.dipendenti\n"
+				+ "INNER JOIN azienda.manager\n"
+				+ "ON  dipendenti.idDipendente = manager.idDipendente\n"
+				+ "WHERE dipendenti.ruolo ='manager';"; 
+		
+		
+	}
 }
--- Gestione_Dipendenti/src/sistema/Menu.java
+++ Gestione_Dipendenti/src/sistema/Menu.java
@@ -3,32 +3,32 @@ package sistema;
 import java.util.Scanner;
 import java.sql.Connection;
 
-public class Menu
-{
+public class Menu {
 	/*
-	 * Metodo che stampa in console un menù con diverse opzioni per gestire i vari sottomenù
+	 * Metodo che stampa in console un menù con diverse opzioni per gestire i vari
+	 * sottomenù
+	 * 
 	 * @param conn: connessione con il database
+	 * 
 	 * @scanner: scanner per prendere testo da console
 	 */
-	public static void menuGenerale(Connection conn, Scanner scanner)
-	{
+	public static void menuGenerale(Connection conn, Scanner scanner) {
 		String risposta;
 		boolean uscita = false; // condizione di ripetizione del ciclo
 
 		System.out.println("Benvenuto!");
 
-		while (!uscita)
-		{
-			System.out.println("\nMenù: \n" + "1. Menù gestione Dipendenti\n" + "2. Menù gestione Manager\n" + "3. Menù gestione Sviluppatore\n" + "4. Esci\n" + "Inserisci comando:");
+		while (!uscita) {
+			System.out.println("\nMenù: \n" + "1. Menù gestione Dipendenti\n" + "2. Menù gestione Manager\n"
+					+ "3. Menù gestione Sviluppatore\n" + "4. Esci\n" + "Inserisci comando:");
 			risposta = scanner.nextLine();
-			switch (risposta)
-			{
+			switch (risposta) {
 			case "1":
 				menuDipendente(conn, scanner);
 				uscita = true;
 				break;
 			case "2":
-				// menuManager(conn,scanner);
+				menuManager(conn, scanner);
 				break;
 			case "3":
 				// menuSviluppatore(conn,scanner);
@@ -45,32 +45,22 @@ public class Menu
 	}
 
 	/*
-	 * Metodo che stampa in console un menù con diverse opzioni per gestire i dipendenti
+	 * Metodo che stampa in console un menù con diverse opzioni per gestire i
+	 * dipendenti
+	 * 
 	 * @param conn: connessione con il database
+	 * 
 	 * @scanner: scanner per prendere testo da console
 	 */
-	public static void menuDipendente(Connection conn, Scanner scanner)
-	{
+	public static void menuDipendente(Connection conn, Scanner scanner) {
 		String risposta;
-		boolean uscita = false; // condizione di ripetizione del ciclo
-		while (!uscita)
-		{
-			System.out.println("\nMenu:\n"
-					+ "1. Inserisci dipendente con team.\n"
-					+ "2. Inserisci dipendente.\n"
-					+ "3. Cancella dipendente.\n"
-					+ "4. Seleziona i dipendenti.\n"
-					+ "5. Seleziona tutti gli impiegati.\n"
-					+ "6. Cambiare team a un dipendente.\n"
-					+ "7. Cambiare stipendio a un dipendente.\n"
-					+ "8. Promuovere in manager un dipendente.\n"
-					+ "9. Torna al menù principale.\n"
-					+ "Inserisci comando: ");
+		boolean uscita = false;
+		while (!uscita) {
+			System.out.println("\nMenu:\n" + "1. Visualizza manager.\n" + "2. Inserisci manager.\n");
 			risposta = scanner.nextLine();
-			switch (risposta)
-			{
+			switch (risposta) {
 			case "1":
-				//Dipendenti.inserimentoDipendenteConTeam(conn, scanner);
+				Dipendenti.inserimentoDipendenteConTeam(conn, scanner);
 				break;
 			case "2":
 				Dipendenti.inserimentoDipendente(conn, scanner);
@@ -100,6 +90,37 @@ public class Menu
 			default:
 				System.out.println("Comando non valido. Riprova.");
 				break;
+			}
+		}
+	}
+
+	public static void menuManager(Connection conn, Scanner scanner) {
+		String risposta;
+		boolean uscita = false; // condizione di ripetizione del ciclo
+		while (!uscita) {
+			System.out.println("\nMenu:\n" + "1. Inserisci dipendente con team.\n" + "2. Inserisci dipendente.\n"
+					+ "3. Cancella dipendente.\n" + "4. Seleziona i dipendenti.\n"
+					+ "5. Seleziona tutti gli impiegati.\n" + "6. Cambiare team a un dipendente.\n"
+					+ "7. Cambiare stipendio a un dipendente.\n" + "8. Promuovere in manager un dipendente.\n"
+					+ "9. Torna al menù principale.\n" + "Inserisci comando: ");
+			risposta = scanner.nextLine();
+			switch (risposta) {
+			case "1":
+				// Dipendenti.inserimentoDipendenteConTeam(conn, scanner);
+				break;
+			case "2":
+				Dipendenti.inserimentoDipendente(conn, scanner);
+				break;
+			case "3":
+				Dipendenti.cancellaDipendente(conn, scanner);
+				break;
+			case "4":
+				Dipendenti.selezioneDipendente(conn);
+				break;
+			case "5":
+				Dipendenti.selezioneImpiegati(conn);
+				break;
+
 			}
 		}
 	}
