58364993200ae71489c58a3d2ac27b5ad9089b4f
Francesco <martinifranky98@gmail.com>
2025-01-15 15:34:06 +0100

    Aggiunti metodi progetti e menu

--- Gestione_Dipendenti/src/sistema/Menu.java
+++ Gestione_Dipendenti/src/sistema/Menu.java
@@ -56,44 +56,87 @@ public class Menu
 		while (!uscita)
 		{
 			System.out.println("\nMenu:\n"
-					+ "1. Inserisci dipendente con team.\n"
-					+ "2. Inserisci dipendente.\n"
-					+ "3. Cancella dipendente.\n"
-					+ "4. Seleziona i dipendenti.\n"
-					+ "5. Seleziona tutti gli impiegati.\n"
-					+ "6. Cambiare team a un dipendente.\n"
-					+ "7. Cambiare stipendio a un dipendente.\n"
-					+ "8. Promuovere in manager un dipendente.\n"
-					+ "9. Torna al menù principale.\n"
+					+ "1. Inserisci dipendente.\n"
+					+ "2. Cancella dipendente.\n"
+					+ "3. Seleziona i dipendenti.\n"
+					+ "4. Seleziona tutti gli impiegati.\n"
+					+ "5. Cambiare team a un dipendente.\n"
+					+ "6. Cambiare stipendio a un dipendente.\n"
+					+ "7. Promuovere in manager un dipendente.\n"
+					+ "8. Torna al menù principale.\n"
 					+ "Inserisci comando: ");
 			risposta = scanner.nextLine();
 			switch (risposta)
 			{
 			case "1":
-				//Dipendenti.inserimentoDipendenteConTeam(conn, scanner);
-				break;
-			case "2":
 				Dipendenti.inserimentoDipendente(conn, scanner);
 				break;
-			case "3":
+			case "2":
 				Dipendenti.cancellaDipendente(conn, scanner);
 				break;
-			case "4":
+			case "3":
 				Dipendenti.selezioneDipendente(conn);
 				break;
-			case "5":
+			case "4":
 				Dipendenti.selezioneImpiegati(conn);
 				break;
-			case "6":
+			case "5":
 				Dipendenti.cambioTeamDipendente(conn, scanner);
 				break;
-			case "7":
+			case "6":
 				Dipendenti.cambioStipendio(conn, scanner);
 				break;
-			case "8":
+			case "7":
 				Dipendenti.promozioneInManager(conn, scanner);
 				break;
-			case "9":
+			case "8":
+				menuGenerale(conn, scanner);
+				uscita = true;
+				break;
+			default:
+				System.out.println("Comando non valido. Riprova.");
+				break;
+			}
+		}
+	}
+	
+	public static void menuProgetti(Connection conn, Scanner scanner)
+	{
+		String risposta;
+		boolean uscita = false; // condizione di ripetizione del ciclo
+		while (!uscita)
+		{
+			System.out.println("\nMenu:\n"
+					+ "1. Inserisci progetto.\n"
+					+ "2. Cancella progetto.\n"
+					+ "3. Seleziona i progetti.\n"
+					+ "4. Cambia nome a un progetto.\n"
+					+ "5. Sostituisci team assegnato a un progetto.\n"
+					+ "6. Cambia project manager di un progetto\n"
+					+ "7. Torna al menù principale.\n"
+					+ "Inserisci comando: ");
+			risposta = scanner.nextLine();
+			switch (risposta)
+			{
+			case "1":
+				Progetti.inserimentoProgetto(conn, scanner);
+				break;
+			case "2":
+				Progetti.cancellaProgetto(conn, scanner);
+				break;
+			case "3":
+				Progetti.selezioneProgetto(conn);
+				break;
+			case "4":
+				Progetti.cambioNomeProgetto(conn, scanner);
+				break;
+			case "5":
+				Progetti.cambioIdTeamAssegnato(conn, scanner);
+				break;
+			case "6":
+				Progetti.cambioProjectManager(conn, scanner);
+				break;
+			case "7":
 				menuGenerale(conn, scanner);
 				uscita = true;
 				break;
